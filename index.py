7d90c4d1c982219266b7ab8b19f7478986417ca0
Tayra <sotomezatayra@gmail.com>
2025-01-04 22:28:43 -0500

    Creación ventana y contenedores: name y price

---
+++ index.py
@@ -0,0 +1,29 @@
+from tkinter import ttk
+from tkinter import *
+
+import sqlite3
+
+class Product:
+
+    def __init__(self, window):
+        self.wind = window
+        self.wind.title('Products Application')
+
+        # Creating a Frame Container
+        frame = LabelFrame(self.wind, text = 'Register A New Product')
+        frame.grid(row = 0, column = 0, columnspan = 3, pady =20)
+
+        # Name Input
+        Label(frame, text = 'Name: ').grid(row = 1, column = 0)
+        self.name = Entry(frame)
+        self.name.grid(row = 1, column = 1)
+
+        # Price Input
+        Label(frame, text = 'Price: ').grid(row = 2, column = 0)
+        self.price = Entry(frame)
+        self.price.grid(row = 2, column = 1)
+
+if __name__ == '__main__':
+    window = Tk()
+    application = Product(window)
+    window.mainloop()
