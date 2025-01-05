db2ace3b65161afa450b4dce285fa2994c693ca5
Tayra <sotomezatayra@gmail.com>
2025-01-04 22:39:04 -0500

    Creación de vista tabla

--- index.py
+++ index.py
@@ -16,6 +16,7 @@ class Product:
         # Name Input
         Label(frame, text = 'Name: ').grid(row = 1, column = 0)
         self.name = Entry(frame)
+        self.name.focus()
         self.name.grid(row = 1, column = 1)
 
         # Price Input
@@ -25,6 +26,12 @@ class Product:
 
         # Button Add Product
         ttk.Button(frame, text = 'Save Product').grid(row = 3, columnspan = 2, sticky = W + E)
+
+        # Table
+        self.tree = ttk.Treeview(height = 10, columns = 2)
+        self.tree.grid(row = 4, column = 0, columnspan = 2)
+        self.tree.heading('#0', text = 'Name', anchor = CENTER)
+        self.tree.heading('#1', text = 'Price', anchor = CENTER)
 
 if __name__ == '__main__':
     window = Tk()
