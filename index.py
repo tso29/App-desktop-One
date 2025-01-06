408f6669bb0450ff40d430f029e86c387b8af5b6
Tayra <sotomezatayra@gmail.com>
2025-01-05 22:36:45 -0500

    Creacion de la tabla

--- index.py
+++ index.py
@@ -5,6 +5,8 @@ import sqlite3
 
 class Product:
 
+    db_name = 'database.db'
+
     def __init__(self, window):
         self.wind = window
         self.wind.title('Products Application')
@@ -25,13 +27,62 @@ class Product:
         self.price.grid(row = 2, column = 1)
 
         # Button Add Product
-        ttk.Button(frame, text = 'Save Product').grid(row = 3, columnspan = 2, sticky = W + E)
+        ttk.Button(frame, text = 'Save Product', command = self.add_product).grid(row = 3, columnspan = 2, sticky = W + E)
+
+        # Output Messages
+        self.message = Label(text = '', fg = 'red')
+        self.message.grid(row = 2, column = 0, columnspan = 2, sticky = W + E)
 
         # Table
         self.tree = ttk.Treeview(height = 10, columns = 2)
         self.tree.grid(row = 4, column = 0, columnspan = 2)
         self.tree.heading('#0', text = 'Name', anchor = CENTER)
         self.tree.heading('#1', text = 'Price', anchor = CENTER)
+
+        # Buttons
+        ttk.Button(text = 'DELETE').grid(row = 5, column = 0, sticky = W + E)
+        ttk.Button(text = 'EDIT').grid(row = 5, column = 1, sticky = W + E)
+
+
+        # Filling the Row
+        self.get_products()
+
+    def run_query(self, query, parameters = ()):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            result = cursor.execute(query, parameters)
+            conn.commit()
+        return result
+
+    def get_products(self):
+        # cleaning table
+        records = self.tree.get_children()
+        for element in records:
+            self.tree.delete(element)
+        # quering data
+        query = 'SELECT * FROM product ORDER BY name DESC'
+        db_rows = self.run_query(query)
+        # filling data
+        for row in db_rows:
+            self.tree.insert('', 0, text = row[1], values = row[2])
+
+    def validation(self):
+        return len(self.name.get()) != 0 and len(self.price.get()) != 0
+
+    def add_product(self):
+        if self.validation():
+            query = 'INSERT INTO product VALUES(NULL, ?, ?)'
+            parameters = (self.name.get(), self.price.get())
+            self.run_query(query, parameters)
+            self.message['text'] = 'Product {} added Succesfully'.format(self.name.get())
+            self.name.delete(0, END)
+            self.price.delete(0, END)
+        else:
+            self.message['text'] = 'Name and Price are Required'
+        self.get_products()
+
+    def delete_product(self):
+
 
 if __name__ == '__main__':
     window = Tk()
